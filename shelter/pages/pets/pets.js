866f96abc81dcb469c9b298eed2b7c4b8ab8211a
Alesia Kuptsova <alesya.kuptsova@tut.by>
2020-10-14 22:43:37 +0200

    fix: fix markup for 320px

--- shelter/pages/pets/pets.js
+++ shelter/pages/pets/pets.js
@@ -7,3 +7,7 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+function myFunction() {
+  document.getElementById("check").classList.toggle("change");
+  document.getElementById("myDropdown").classList.toggle("show");
+}
