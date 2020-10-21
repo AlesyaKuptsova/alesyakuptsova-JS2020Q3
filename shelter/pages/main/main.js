fc48c7780fc88dcf26602fd7853022c9d7c4f75a
Alesia Kuptsova <alesya.kuptsova@tut.by>
2020-10-21 19:38:06 +0200

    feat: start working on DOM

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -2,3 +2,15 @@ function myFunction() {
     document.getElementById("check").classList.toggle("change");
     document.getElementById("myDropdown").classList.toggle("show");
 }
+document.addEventListener("click", function(e) {
+    let m = document.getElementById('menu');
+    let a = document.getElementById('backdrop');
+    if (e.target.id != 'hmt' && e.target.id != 'menu') {
+      m.style.display = 'none';
+      a.style.display = 'none';
+      m.style.right
+    } else if (e.target.id == 'hmt') {
+      m.style.display = (m.style.display != 'block') ? 'block' : 'none';
+      a.style.display = (a.style.display != 'block') ? 'block' : 'none';
+    }
+  });
