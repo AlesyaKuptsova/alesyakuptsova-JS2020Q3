50c837b5b1b59967d5e79eb38b64c6a0c130f64e
Alesia Kuptsova <alesya.kuptsova@tut.by>
2020-12-06 23:44:14 +0100

    feat: updade hamburger menu

--- english-for-kids/src/components/router.js
+++ english-for-kids/src/components/router.js
@@ -1,61 +1,64 @@
 'use strict';
 
 export function Router(routes) {
-    try {
-        if (!routes) {
-            throw 'error: routes param is mandatory';
-        }
-        this.constructor(routes);
-        this.init();
-    } catch (e) {
-        console.error(e);
+  try {
+    if (!routes) {
+      throw 'error: routes param is mandatory';
     }
+    this.constructor(routes);
+    this.init();
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 Router.prototype = {
-    routes: undefined,
-    rootElem: undefined,
-    constructor: function (routes) {
-        this.routes = routes;
-        this.rootElem = document.getElementById('app');
-    },
-    init: function () {
-        var r = this.routes;
-        (function(scope, r) {
-            window.addEventListener('hashchange', function (e) {
-                scope.hasChanged(scope, r);
-            });
-        })(this, r);
-        this.hasChanged(this, r);
-    },
-    hasChanged: function(scope, r){
-        if (window.location.hash.length > 0) {
-            for (var i = 0, length = r.length; i < length; i++) {
-                var route = r[i];
-                if(route.isActiveRoute(window.location.hash.substr(1))) {
-                    scope.goToRoute(route.htmlName);
-                }
-            }
-        } else {
-            for (var i = 0, length = r.length; i < length; i++) {
-                var route = r[i];
-                if(route.default) {
-                    scope.goToRoute(route.htmlName);
-                }
-            }
+  routes: undefined,
+  rootElem: undefined,
+  onLoad: undefined,
+  constructor(routes, onLoad) {
+    this.routes = routes;
+    this.onLoad = onLoad;
+    this.rootElem = document.getElementById('app');
+  },
+  init() {
+    const r = this.routes;
+    (function (scope, r) {
+      window.addEventListener('hashchange', (e) => {
+        scope.hasChanged(scope, r);
+      });
+    }(this, r));
+    this.hasChanged(this, r);
+  },
+  hasChanged(scope, r) {
+    if (window.location.hash.length > 0) {
+      for (let i = 0, { length } = r; i < length; i++) {
+        var route = r[i];
+        if (route.isActiveRoute(window.location.hash.substr(1))) {
+          scope.goToRoute(route.htmlName);
         }
-    },
-    goToRoute: function (htmlName) {
-        (function(scope) {
-            var url = 'pages/' + htmlName,
-                xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState === 4 && this.status === 200) {
-                    scope.rootElem.innerHTML = this.responseText;
-                }
-            };
-            xhttp.open('GET', url, true);
-            xhttp.send();
-        })(this);
+      }
+    } else {
+      for (let i = 0, {length} = r; i < length; i++) {
+        var route = r[i];
+        if (route.default) {
+          scope.goToRoute(route.htmlName);
+        }
+      }
     }
+  },
+  goToRoute(htmlName) {
+    (function (scope) {
+      let url = 'pages/' + htmlName;
+      let xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+          scope.rootElem.innerHTML = this.responseText;
+          scope.onLoad(scope.rootElem);
+        }
+      };
+      xhttp.open('GET', url, true);
+      xhttp.send();
+    }(this));
+  },
 };
